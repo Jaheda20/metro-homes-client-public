311be63db58cfdbf0fbed81e6f23934d435025cc
Jaheda20 <jahedasultana20@gmail.com>
2024-06-08 09:36:45 +0200

    implemented status change on offers

---
+++ src/Components/Table/OfferedDataRow.jsx
@@ -0,0 +1,87 @@
+import { SiTicktick } from "react-icons/si";
+import { GiCancel } from "react-icons/gi";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+
+
+
+const OfferedDataRow = ({ user, refetch, offers }) => {
+
+    const axiosSecure = useAxiosSecure();
+    const { mutateAsync } = useMutation({
+        mutationFn: async status => {
+            const { data } = await axiosSecure.patch(`/offers/status/${offers?._id}`, status)
+            return data
+        },
+        onSuccess: data => {
+            refetch()
+            console.log(data)
+            toast.success('Offer is accepted!')
+        },
+    })
+
+    const handleAccept = async () => {
+        try {
+            await mutateAsync({ status: 'Accepted' })
+        }
+        catch (err) {
+            console.log(err.message)
+            toast.error(err.message)
+        }
+    }
+
+    const handleReject = async () => {
+        try {
+            await mutateAsync({ status: 'Rejected' })
+        }
+        catch (err) {
+            console.log(err.message)
+            toast.error(err.message)
+        }
+    }
+
+    return (
+        <tr>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.title}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.location}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.email}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.buyerName}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.amount}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <p className='text-gray-900 whitespace-no-wrap'>{offers?.status}</p>
+            </td>
+            <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                <div className='text-gray-900 whitespace-no-wrap flex flex-col gap-2'>
+                    <button onClick={handleAccept} className="text-blue-700 btn"> <SiTicktick size={20} />
+                    </button>
+                    <button onClick={handleReject} className="text-red-700 btn"> <GiCancel size={20} />
+                    </button>
+
+
+
+                </div>
+            </td>
+
+
+
+
+
+
+
+        </tr>
+    );
+};
+
+export default OfferedDataRow;
--- src/Pages/Dashboard/Agent/RequestedProperties.jsx
+++ src/Pages/Dashboard/Agent/RequestedProperties.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import useAuth from "../../../Hooks/useAuth";
+import OfferedDataRow from "../../../Components/Table/OfferedDataRow";
 
 const RequestedProperties = () => {
     const axiosSecure = useAxiosSecure();
@@ -24,6 +25,67 @@ const RequestedProperties = () => {
     return (
         <div>
             <h2 className="text-2xl font-semibold my-10">Requested/Offered: ({gotOffers.length})</h2>
+            <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+              <table className='min-w-full leading-normal'>
+                <thead>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Property Title
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Location
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Buyer Email
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Buyer Name
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Offered price ($)
+                    </th>
+                    <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      Status
+                    </th>
+                    {/* <th
+                      scope='col'
+                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                    >
+                      
+                    </th> */}
+
+                   
+                  </tr>
+                </thead>
+                <tbody>                  
+                  {gotOffers.map(offers => <OfferedDataRow key={offers._id}
+                  offers = {offers}
+                  user={user}
+                  refetch = {refetch}>
+                  </OfferedDataRow>)}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
     );
 };
